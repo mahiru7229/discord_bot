5c3c5311ce4774efd29d6102df97543c3afdc263
mahiru7229 <110292918+mahiru7229@users.noreply.github.com>
2023-01-14 20:06:23 +0700

    nothing

--- .py
+++ .py
@@ -7,3 +7,9 @@
     
 # logging.basicConfig(filename=os.path.join("code","log","log.txt"), level=logging.DEBUG,format="{} %(message)s".format(get_time()))
 # logging.debug("Debug logging test...")
+
+
+
+
+# User = await client.fetch_user(DISCORD_ID_HERE)
+# print(User.avatar) # this will print the link to the users profile picture!
